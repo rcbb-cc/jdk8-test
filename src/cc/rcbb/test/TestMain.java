729c8eaff25f3007ab86686ab064a6e28ea7c917
lvhaosir <lvhao@umeox.com>
2021-09-01 16:28:26 +0800

    Initial commit

---
+++ src/cc/rcbb/test/TestMain.java
@@ -0,0 +1,12 @@
+package cc.rcbb.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("one", "1");
+        map.put("two", "2");
+    }
+}
